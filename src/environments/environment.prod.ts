e4f05adaec4800aee590ddf58a17c9473cad13eb
unknown <efrenmedina@live.com>
2019-05-01 14:44:26 -0400

    heroku aster

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,9 +7,9 @@ switch (window.location.hostname) {
     // this is the deployed angular application
     case 'https://dogbreedproject.herokuapp.com/':
         // this is the full url of your deployed API
-        APIURL = 'http://localhost:3000'
+        APIURL = 'https://marcusverdeflorhaynesserver.herokuapp.com'
         break;
     default:
         // this is the local host name of your API
-        APIURL = 'http://localhost:3000';
+        APIURL = 'https://marcusverdeflorhaynesserver.herokuapp.com';
 }
